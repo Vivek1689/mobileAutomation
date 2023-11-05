52e51c7638cf834a7a8e364b60a8b2ae0de4ca28
VivekVitthalShetty <vivekshetty89@gmail.com>
2023-11-05 12:44:52 +0530

    Updated java client dependency

--- src/main/java/basePackage/BaseClass.java
+++ src/main/java/basePackage/BaseClass.java
@@ -10,23 +10,23 @@ import java.net.URL;
 
 public class BaseClass {
 
-    public AndroidDriver androidDriver;
+    public static AndroidDriver androidDriver;
 
     @BeforeClass
     public void configureAppium() throws MalformedURLException {
         UiAutomator2Options uiAutomator2Options = new UiAutomator2Options();
-        uiAutomator2Options.setDeviceName("VivekPixel");
-       // uiAutomator2Options.setDeviceName("Pixel 7 Pro API 28");
+        //uiAutomator2Options.setDeviceName("VivekPixel");
+        uiAutomator2Options.setDeviceName("Pixel 7 Pro API 28");
        // uiAutomator2Options.setAppPackage("com.google.android.youtube");
 
         //Configuring app in uiAutomator
         //uiAutomator2Options.setApp("/Users/VivekVitthalShetty/IdeaProjects/untitled/src/test/java/resources/ApiDemos-debug.apk");
 
-        androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"),uiAutomator2Options);
         uiAutomator2Options.setApp("/Users/VivekVitthalShetty/IdeaProjects/untitled/src/test/java/resources/General-Store.apk");
+        androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"),uiAutomator2Options);
+        //uiAutomator2Options.setApp("/Users/VivekVitthalShetty/IdeaProjects/untitled/src/test/java/resources/General-Store.apk");
 
     }
-
 
     @AfterClass
     public void tearDown(){
